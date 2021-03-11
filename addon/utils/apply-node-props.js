7dbcaac9c5d37a150da62c6291440fe8e10e6dc4
Matej Janjic <matej.janjic@productive.io>
2021-03-11 12:00:52 +0100

    [feature] add strict mode support

--- addon/utils/apply-node-props.js
+++ addon/utils/apply-node-props.js
@@ -1,27 +1,39 @@
+// adopted from: https://github.com/konvajs/react-konva/blob/master/src/makeUpdates.js
+
+import config from 'ember-get-config';
 import updatePicture from 'ember-konva/utils/update-picture';
 
 const propsToSkip = { parent: true, didInsert: true, willDestroy: true };
 const EVENTS_NAMESPACE = 'ember-konva-event';
+const useStrictMode = config['ember-konva'].strictMode;
 
 export default function applyNodeProps(instance, props = {}, oldProps = {}) {
-  const updatedProps = {};
-  let hasUpdates = false;
-
   for (let key in oldProps) {
     if (propsToSkip[key]) {
       continue;
     }
     const isEvent = key.slice(0, 2) === 'on';
     const hasPropChanged = oldProps[key] !== props[key];
+
     if (isEvent && hasPropChanged) {
-      const eventName = key.substr(2).toLowerCase();
-      instance.off(`${eventName}.${EVENTS_NAMESPACE}`, oldProps[key]);
+      let eventName = key.substr(2).toLowerCase();
+      if (eventName.substr(0, 7) === 'content') {
+        eventName = `content${eventName.substr(7, 1).toUpperCase()}${eventName.substr(8)}`;
+      }
+      instance.off(eventName, oldProps[key]);
     }
+
     const toRemove = !props.hasOwnProperty(key);
     if (toRemove) {
       instance.setAttr(key, undefined);
     }
   }
+
+  let hasUpdates = false;
+  const strictUpdate = useStrictMode || props._useStrictMode;
+  const updatedProps = {};
+  const newEvents = {};
+
   for (let key in props) {
     if (propsToSkip[key]) {
       continue;
@@ -29,20 +41,32 @@ export default function applyNodeProps(instance, props = {}, oldProps = {}) {
     const isEvent = key.slice(0, 2) === 'on';
     const shouldAddProp = oldProps[key] !== props[key];
     if (isEvent && shouldAddProp) {
-      const eventName = key.substr(2).toLowerCase();
+      let eventName = key.substr(2).toLowerCase();
+      if (eventName.substr(0, 7) === 'content') {
+        eventName = `content${eventName.substr(7, 1).toUpperCase()}${eventName.substr(8)}`;
+      }
+
       if (props[key]) {
-        instance.off(`${eventName}.${EVENTS_NAMESPACE}`);
-        instance.on(`${eventName}.${EVENTS_NAMESPACE}`, props[key]);
+        newEvents[eventName] = props[key];
       }
     }
-    if (!isEvent && props[key] !== oldProps[key]) {
+    if (
+      !isEvent &&
+      (props[key] !== oldProps[key] ||
+      (strictUpdate && props[key] !== instance.getAttr(key)))
+    ) {
       hasUpdates = true;
       updatedProps[key] = props[key];
     }
   }
 
   if (hasUpdates) {
+    console.log(updatedProps)
     instance.setAttrs(updatedProps);
     updatePicture(instance);
+  }
+
+  for (let eventName in newEvents) {
+    instance.on(`${eventName}.${EVENTS_NAMESPACE}`, newEvents[eventName]);
   }
 }
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  _getAddonOptions() {
+    const parentOptions = this.parent && this.parent.options;
+    const appOptions = this.app && this.app.options;
+
+    return parentOptions || appOptions || {};
+  },
+
+  config(env, baseConfig) {
+    const addonOptions = this._getAddonOptions();
+    const customOptions = addonOptions['ember-konva'];
+
+    const config = {
+      'ember-konva': {
+        strictMode: Boolean(customOptions?.strictMode)
+      }
+    };
+
+    const updatedConfig = Object.assign({}, baseConfig, config);
+
+    return updatedConfig;
+  }
 };
